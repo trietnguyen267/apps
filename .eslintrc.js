617d19ea971787e10aae27be91b66f836cf409a3
Ben Gao <xingbingao@gmail.com>
2020-01-08 15:57:18 +1300

    Try cennznet/types

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
   ...base,
   parserOptions: {
     ...base.parserOptions,
-    project: [
-      './tsconfig.json'
-    ]
+    project: ['./tsconfig.json']
   },
   rules: {
     ...base.rules,
+    'prettier/prettier': 'error',
     '@typescript-eslint/no-explicit-any': 'off'
-  }
+  },
+  plugins: ['prettier'],
+  extends: ['prettier']
 };
